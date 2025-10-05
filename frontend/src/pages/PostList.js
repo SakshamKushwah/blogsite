d8e3b1433366371eea0eef78f22fd547dae1a59d
Saksham kushwah <eashuop91@gmail.com>
2025-10-05 11:03:24 +0530

    h

--- frontend/src/pages/PostList.js
+++ frontend/src/pages/PostList.js
@@ -60,7 +60,7 @@ export default function PostList() {
             {p.imageUrl && (
               <img
                 className="post-card-img"
-                src={`https://blogsite-back.vercel.app/${p.imageUrl.replace(/^\/+/, "")}`}
+                src={`https://blogsite-9nr5-b.vercel.app/${p.imageUrl.replace(/^\/+/, "")}`}
                 alt={p.title}
               />
             )}
@@ -90,7 +90,7 @@ export default function PostList() {
             {selectedPost.imageUrl && (
               <img
                 className={`modal-img ${selectedPost.imageAlignment || "left"}`}
-                src={`https://blogsite-back.vercel.app/p${selectedPost.imageUrl.replace(/^\/+/, "")}`}
+                src={`https://blogsite-9nr5-b.vercel.app/${selectedPost.imageUrl.replace(/^\/+/, "")}`}
                 alt={selectedPost.title}
               />
             )}
